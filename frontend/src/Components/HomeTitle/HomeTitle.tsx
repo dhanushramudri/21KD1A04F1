f44d9c89d0546316669f3ec278a3a7441e6704cd
dhanushramudri <ramudridhanush@gmail.com>
2024-07-31 15:57:35 +0530

    Added Screenshots

--- frontend/src/Components/HomeTitle/HomeTitle.tsx
+++ frontend/src/Components/HomeTitle/HomeTitle.tsx
@@ -3,18 +3,13 @@ import EcommerceImg from "../ecommerce.png";
 
 export default function HomeTitle() {
   return (
-    <div
-      className="w-full mt-16 flex items-center flex-col"
-      // style={{
-      //   backgroundImage: `url(${EcommerceImg})`,
-      //   backgroundRepeat: "no-repeat",
-      //   backgroundPosition: "center",
-      //   backgroundSize: "cover",
-      // }}
-    >
-      <div className="text-4xl font-bold text-center">
-        Revolutionize Your <span className="text-[#0077b6]">Shopping </span>
+    <div className="w-full mt-16 flex items-center flex-col p-4">
+      <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-4">
+        Revolutionize Your <span className="text-[#0077b6]">Shopping</span>{" "}
         Experience
+      </div>
+      <div className="text-base sm:text-lg md:text-xl lg:text-2xl text-center text-gray-700">
+        Discover a new way to shop, tailored just for you.
       </div>
     </div>
   );
